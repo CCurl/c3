e4602793682cba115371cbea4927734963e822b0
Chris Curl <curlbud-github.com>
2023-10-03 21:51:36 -0400

    increase version

--- c3.c
+++ c3.c
@@ -43,7 +43,7 @@ enum { // Floating point opcdes
     SQRT, TANH
 };
 
-enum { STOP_LOAD = 99, ALL_DONE = 999, VERSION = 90 };
+enum { STOP_LOAD = 99, ALL_DONE = 999, VERSION = 92 };
 
 #define BTW(a,b,c)    ((b<=a) && (a<=c))
 #define CELL_SZ       sizeof(cell_t)
